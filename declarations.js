44dfb6ce9b31e18fc2802f8a9697f68c05255743
Zara Masino <masinoz@merrimack.edu>
2022-12-04 16:33:15 -0500

    separated into more files

---
+++ declarations.js
@@ -0,0 +1,112 @@
+var audio;
+let caughtSound = ['sounds/catch/creatureGrowl.wav', 'sounds/catch/groan.wav', 'sounds/catch/scaryRoar.wav'];
+let closeSound = ['sounds/close/roar.wav', 'sounds/close/close1.wav', 'sounds/close/close2.wav', 'sounds/close/close3.wav'];
+let loseSounds = ['sounds/gameOver/lose1.wav', 'sounds/gameOver/lose2.wav'];
+let thunderNoises = ['sounds/thunder1.wav', 'sounds/thunder2.wav', 'sounds/thunder3.wav', 'sounds/thunder4.wav',];
+let heartbeatAudio = new Audio('sounds/heartbeat.wav');
+
+let currentTime = 0;
+let shootingStars = [];
+let caught = false;
+let distance = 25;
+let timer = 0;
+let monsterSpeed = .08;
+let stamina = 100;
+let maxStamina = 100;
+let fitness = .5;
+let gameTime = 0;
+let increment = 2;
+let level = "easy";
+let gamePaused = false;
+let gameState = 0;
+let starsCollected = 0;
+let lives = 3;
+let spotLightOn = false;
+let gameOverBool = false;
+let x = 0;
+let y = 50;
+let scales;
+let timerThunder = 0;
+let maxThunder = 0;
+let k = .03;
+
+ // particles 
+ let movementSpeeds = 40;
+ let totalObjects = 500;
+ let objectSize = 10;
+ let colors = [0xFF0FFF, 0xCCFF00, 0xFF000F, 0x996600, 0xFFFFFF, 0xcbbeb5, 0x9003ff, 0x6d9bc3,
+     0x87a96b, 0x520520, 0xeaa315, 0x327572, 0xb8888d, 0xcc77dd, 0xbcd4e6, 0xfe6f5e, 0x318ce7, 0xa2a2d0];
+ let dirs = [];
+ let parts = [];
+
+ // smoke 
+ let smokeParticles;
+ let smokeTexture;
+ let smokeMaterial;
+ let smokeGeo
+
+
+
+ // used for movement and rotation management
+ let speed = 2;
+ let delta;
+
+ // used for the head bobbing effect
+ let change = 0;
+ let sinwave = 0;
+ let displacement = 0;
+
+ // used for the cursor placement
+ let toggle = 0;
+
+ // used in movement and collision
+ let hit = false;
+ let moving = false;
+ let starHit = -1;
+
+ let intersection = null;
+
+
+ let trees = [];
+ let spheres = [];
+ let worldItems = [];
+ let starShards = [];
+ let heartArray = [];
+ let movingStarShard = [];
+ let starXDirs = [];
+ let starZDirs = [];
+ let prettyStars = [];
+
+
+ // used in movement and collision
+ let movement;
+ let oldCamera;
+
+ // geometries, materials, and meshes
+ let worldGeom;
+ let worldMat;
+ let world;
+
+ let waterGeom;
+ let waterMat;
+ let water;
+
+ let terrainGeom;
+ let terrainMat;
+ let terrain;
+
+ let terrainShadowMat;
+ let terrainShadow;
+
+ let cursorGeom;
+ let cursorMat;
+ let cursor;
+
+
+ let moon;
+
+  //Colors for sky/sun/moon
+  let moonColor = [0xdde2e5];
+  let skyColors = [0x000608, 0x072f41, 0x094f6d, 0x436e91, 0x5e88b8, 0x7998bb];
+
+  let spotLight, lightTarget;
---
+++ functions.js
@@ -0,0 +1,37 @@
+    //This function randomly returns a true or false value or a number between 0 and 10 in order to help randomize certain customizations
+    function random() {
+        let randomNumber = Math.round(Math.random() * 10); //Creates a random number between 0 and 10
+        if (randomNumber % 2 == 0) { //If the random number is even
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+
+    function createRandomPosOrNeg(max, min) {
+        let number = Math.random() * max + min
+        if (random() == true) {
+            number *= -1;
+        }
+        return number;
+    }
+
+    function play(path) {
+        audio = new Audio(path);
+        audio.volume = .5;
+        audio.play();
+    }
+
+
+          //src:https://developer.mozilla.org/en-US/docs/Games/Techniques/3D_collision_detection
+          function calcPointDistance(point, point2) {
+            //This calculates the distance of two points based on the distance formula
+            const distance = Math.sqrt(
+                (point.x - point2.x) * (point.x - point2.x) +
+                (point.y - point2.y) * (point.y - point.y) +
+                (point.z - point2.z) * (point.z - point2.z)
+            )
+            return distance; //Returns distance
+        }
